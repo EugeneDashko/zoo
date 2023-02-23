cacc9c56a8ea74589ce91f08632dc7f65737be44
EugeneDashko <eugenedashko@yahoo.com>
2023-02-23 11:58:26 +0300

    feat: finish header& benifits

---
+++ js/app.js
@@ -0,0 +1,12 @@
+import * as flsFunction from "./modules/functions.js"
+flsFunction.isWebp();
+//============================================================================================
+console.log('hi');
+
+  // const init = () => {
+  //   renderListProduct();
+  //   navigationListController();
+  //   cartInit();
+  // };
+  // init();
+
