ca9101bda0a97d3ad1904f0fee7d654dfbccdbed
EugeneDashko <eugenedashko@yahoo.com>
2023-03-01 16:53:16 +0300

    working on footer

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,6 @@
 import * as flsFunction from "./modules/functions.js"
 flsFunction.isWebp();
-//============================================================================================
-console.log('hi');
 
-  // const init = () => {
-  //   renderListProduct();
-  //   navigationListController();
-  //   cartInit();
-  // };
-  // init();
+const swiper = new Swiper();
+//============================================================================================
 
